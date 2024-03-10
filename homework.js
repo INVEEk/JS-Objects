e60e8bcbcdbf14fc66f74b441c4e499c96f691b9
Maciej <maciej.pieciuk@gmail.com>
2024-03-10 23:18:06 +0100

    Portion of exercise from homework

--- homework.js
+++ homework.js
@@ -184,3 +184,93 @@ function getCityInformation() {
 }
 
 console.log(getCityInformation());
+
+/* #8
+Write the getUserCopy function. Assume that users only have the firstName and lastName.
+ */
+const john = {
+    firstName: 'John',
+    lastName: 'Smith'
+}
+
+const newUser = getUserCopy(john);
+
+console.log(newUser.firstName); // John
+console.log(newUser.lastName); // Smith
+console.log(newUser === john); // false <-- this is crucial
+
+/* #9 https://www.codewars.com/kata/571f1eb77e8954a812000837/train/javascript
+Give you a function animal, accept 1 parameter:obj like this:
+
+{name:"dog",legs:4,color:"white"}
+and return a string like this:
+
+"This white dog has 4 legs."
+ */
+
+const minion = {
+    name: "dog",
+    legs: 4,
+    color: "white"
+}
+
+function animal() {
+    return `This ${minion.color} ${minion.name} has ${minion.legs} legs.`
+}
+
+console.log(animal());
+
+/* #10 https://www.codewars.com/kata/56d8ae9237123036d3001b54
+While making a zork-type game, you create an object of rooms.
+Unfortunately, the game is not working.
+Find all the errors in the rooms object to get your game working again.
+
+var rooms = {
+  first: {
+    description: 'This is the first room'
+    items: {
+      chair: 'The old chair looks comfortable',
+      lamp: 'This lamp looks ancient'
+  },
+  second: {
+    description: 'This is the second room'
+    items: {
+      couch: 'This couch looks like it would hurt your back,
+      table: 'On the table there is an unopened bottle of water'
+    }
+  }
+}
+ */
+
+const rooms = {
+    first: {
+        description: 'This is the first room',
+        items: {
+            chair: 'The old chair looks comfortable',
+            lamp: 'This lamp looks ancient'
+        }
+    },
+    second: {
+        description: 'This is the second room',
+        items: {
+            couch: 'This couch looks like it would hurt your back',
+            table: 'On the table there is an unopened bottle of water'
+        }
+    }
+}
+
+/* #11 Write the addContact function that returns a new contacts object with an additional property.
+The new property should contain an object with the name and phoneNumber properties.
+Make sure not to modify the contacts provided as an argument to the addContact function.
+ */
+
+const contacts = {};
+const contactsWithJohn = addContact(contacts, 'John', '123 456 789');
+
+console.log(contactsWithJohn.John.name); // "John"
+console.log(contactsWithJohn.John.phoneNumber); // '123 456 789'
+console.log(contacts === contactsWithJohn); // false
+console.log(contacts.John); // undefined
+
+const johnContactInfo = findContact(contactsWithJohn, 'John');
+console.log(johnContactInfo); // "Name: John, Phone: 123 456 789"
