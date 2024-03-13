b751dc5ba31de491645167a1127da37731def16a
Maciej <maciej.pieciuk@gmail.com>
2024-03-13 23:37:03 +0100

    Make cnahges on few exercise

--- Exercises.js
+++ Exercises.js
@@ -1,7 +1,12 @@
 /*
 Write the checkIfPropertyIsDefined function, which checks if the value of the property isn't undefined.
+*/
 
- */
+const car = {
+    color: 'green',
+    lastName: 'Smith',
+    age: undefined,
+}
 
 const secondUser = {
     firstName: 'Malfoey',
@@ -15,11 +20,165 @@ const firstUser = {
     age: undefined,
 }
 
-function checkIfPropertyIsDefined(user, property) {
-    return user[property] !== undefined;
+function checkIfPropertyIsDefined(someObject, property) {
+    return someObject[property] !== undefined;
 }
 
 console.log(checkIfPropertyIsDefined(firstUser, 'firstName')); // true
 console.log(checkIfPropertyIsDefined(firstUser, 'age')); // false
 console.log(checkIfPropertyIsDefined(firstUser, 'height')); // false
 console.log(checkIfPropertyIsDefined(secondUser, 'firstName')); // true
+console.log(checkIfPropertyIsDefined(car, 'firstName')); // true
+
+/* #2
+Add up all the salaries
+*/
+
+const salaries = {
+    john: 50,
+    adam: 100,
+    katie: 75
+}
+const salarySum = salaries.john + salaries.adam + salaries.katie;
+
+console.log(salarySum);
+
+/* #3
+Write the isObject function. Use the typeof operator.
+isObject({}); // true
+isObject(10); // false
+isObject(null); // false
+*/
+
+function isObject(parameter) {
+    return typeof parameter === 'object' && parameter !== null;
+}
+
+console.log(isObject({})); // true
+console.log(isObject(10)); // false
+console.log(isObject(null)); // false
+
+/* #4
+Write the removeProperty function. It should return true  if there is a property with a given key to remove. Otherwise, it should return false.
+*/
+
+const user = {
+    name: 'John'
+}
+
+function removeProperty(holder, property) {
+    return holder[property] !==undefined && delete user.name;
+}
+
+console.log(removeProperty(user, 'name')); // true
+console.log(removeProperty(user, 'name')); // false
+console.log(removeProperty(user, 'weight')); // false
+console.log(user); // {}
+
+/* #5
+Write the checkIfUsersHaveTheSameName function
+*/
+
+const firstJohn = {
+    firstName: 'John',
+    lastName: 'Smith'
+}
+
+const secondJohn = {
+    firstName: 'John',
+    lastName: 'Smith'
+}
+
+const adam = {
+    firstName: 'Adam',
+    lastName: 'Johnson'
+}
+
+function checkIfUsersHaveTheSameName(sourceUser, userToCheck) {
+    return sourceUser.firstName === userToCheck.firstName;
+}
+
+console.log(checkIfUsersHaveTheSameName(firstJohn, secondJohn)); // true
+console.log(checkIfUsersHaveTheSameName(firstJohn, adam)); // false
+
+/* #6
+Write the getVoteCount function
+
+const votes = {
+    upvotes: 10,
+    downvotes: 5
+}
+*/
+
+function getVoteCount({upvotes, downvotes}) {
+    return upvotes - downvotes;
+}
+
+console.log(getVoteCount({ upvotes: 10, downvotes: 5 })); // 5
+console.log(getVoteCount({ upvotes: 75, downvotes: 90 })); // -15
+console.log(getVoteCount({ upvotes: 50, downvotes: 50 })); // 0+
+
+/* #7
+Write the getCubeVolume function
+*/
+
+function getCubeVolume({width, length, height}) {
+    return width * length * height;
+}
+
+console.log(getCubeVolume({ width: 10, length: 5, height: 2 })); // 100
+console.log(getCubeVolume({ width: 100, length: 500, height: 0 })); // 0
+console.log(getCubeVolume({ width: 15, length: 2, height: 5 })); // 150
+
+/* #8
+Write the getCityInformation function, use template literals
+*/
+
+const cityInformation = {
+    name: 'Warsaw',
+    country: 'Poland',
+    areaInKilometers: 517,
+}
+
+function getCityInformation(cityName, countryName, territory) {
+    return `${cityInformation.name} is in ${cityInformation.country} and has an area of ${cityInformation.areaInKilometers} square kilometers.`;
+}
+
+/* #10 https://www.codewars.com/kata/56d8ae9237123036d3001b54
+While making a zork-type game, you create an object of rooms.
+Unfortunately, the game is not working.
+Find all the errors in the rooms object to get your game working again.
+
+var rooms = {
+  first: {
+    description: 'This is the first room'
+    items: {
+      chair: 'The old chair looks comfortable',
+      lamp: 'This lamp looks ancient'
+  },
+  second: {
+    description: 'This is the second room'
+    items: {
+      couch: 'This couch looks like it would hurt your back,
+      table: 'On the table there is an unopened bottle of water'
+    }
+  }
+}
+ */
+
+const rooms = {
+    first: {
+        description: 'This is the first room',
+        items: {
+            chair: 'The old chair looks comfortable',
+            lamp: 'This lamp looks ancient'
+        }
+    },
+    second: {
+        description: 'This is the second room',
+        items: {
+            couch: 'This couch looks like it would hurt your back',
+            table: 'On the table there is an unopened bottle of water'
+        }
+    }
+}
